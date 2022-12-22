43b2e1cc4591011deccc67041b2f506761fba793
unknown <textile.zero@gmail.com>
2022-12-22 07:20:07 +0600

    exclusive live course layout , cards added nyewly on hompage

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,11 +2,23 @@ import React, { useContext } from 'react';
 import { CategoryContext } from '../../App';
 import Banner from '../Shared/Banner/Banner';
 import HomeCatDesigner from '../Shared/HomeCatDesigner/HomeCatDesigner';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import SingleCourse from '../SingleCourse/SingleCourse';
 
 
 const Home = () => {
     let categories = useContext(CategoryContext);
     categories = categories.filter(category => category.category_id !== 10);
+    const [livecourse, setLiveCourse] = useState([]);
+    useEffect(() => {
+        fetch("https://eduaid-server-side.vercel.app/course-type/live")
+            .then(res => res.json())
+            .then(data => {
+                const courses = data.slice(1, 4)
+                setLiveCourse(courses)
+            })
+    }, [])
     return (
         <div>
             <Banner></Banner>
@@ -20,6 +32,18 @@ const Home = () => {
                         ></HomeCatDesigner>)
                     }
                 </div>
+            </div>
+            <div className='mx-auto px-5' >
+                <h2 className='my-5'>Exclusive Live Courses</h2>
+                <div className='row row-cols-1 row-cols-lg-3 gy-lg-4 px-5'>
+                    {
+                        livecourse.map(course => <SingleCourse
+                            key={course._id}
+                            course={course}
+                        ></SingleCourse>)
+                    }
+                </div>
+
             </div>
         </div>
     );
